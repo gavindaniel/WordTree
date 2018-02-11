852a7638426289e7b1714f33ef24acc7cdcfc77d
Gavin Daniel <gavindaniel@email.arizona.edu>
2018-02-11 16:06:59 -0700

    adding new files

---
+++ src/model/Word.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Word {
+
+	String element;		// content of the Word (e.g., element = "Bee")
+	Word parentWord;		// parent node in the binary tree
+	Word leftChild;		// left child node in the binary tree
+	Word rightChild;		// right child node in the binary tree
+	
+}
---
+++ src/model/WordTree.java
@@ -0,0 +1,8 @@
+package model;
+
+public class WordTree {
+
+	int height;		// height of the binary tree
+	Word rootWord;	// root node of the binary tree
+	
+}
